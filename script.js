0100cf737d5aaad135d8dd746166a298412fbf27
Jordan Villafania <jovillafania@s.sfusd.edu>
2018-01-18 01:22:07 +0000

    1/17/18

--- script.js
+++ script.js
@@ -11,24 +11,26 @@ $(document).ready(function() {
     // This function should return true if the input word starts with a vowel,
     // otherwise it should return false.
     function wordStartsWithVowel(word) {
-
+            if (word.charAt(0) === "a" || word.charAt(0) === "e" || word.charAt(0) === "i" || word.charAt(0) === "o" || word.charAt(0) === "u") {
+        return(word + "yay");
+    }
     }
 
     // Appends "yay" to the end of the word and returns the word.
     function appendYayToWord(word) {
-
+         return word + "yay";
     }
 
     // Moves the first consonant to the end of the word, appends "ay" to the end of the word,
     // and returns the word.
     function convertWordWithConsonant(word) {
-
+        return(word.substring(1) + word.charAt(0) + "ay");
     }
 
     // If the word starts with a vowel, return the result of appendYayToWord.
     // Otherwise, return the result of convertWordWithConsonant.
     function convertWordToPigLatin(word) {
-
+        
     }
 
     // Create a click handler that takes is triggered when the user clicks the translate button.
